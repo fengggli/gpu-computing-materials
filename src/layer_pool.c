6b67028a8a189dd925a3cf2d003f83ae4d49d012
fuyuan <fuyuan@iupui.edu>
2019-03-19 19:13:18 -0400

    add back

--- src/layer_pool.c
+++ src/layer_pool.c
@@ -3,17 +3,33 @@
 
 // y: N, C, 1, 1
 status_t global_avg_pool_forward(tensor_t const x, lcache_t *cache, tensor_t y){
-	uint num_images = x.dim.dims[0];
-	uint num_channels = x.dim.dims[1];
+  uint num_images = x.dim.dims[0];
+  uint num_channels = x.dim.dims[1];
 
-	for (uint i = 0; i < num_images; ++i)
-	  for (uint j = 0; j < num_channels; ++j){
-			int tmp = i * num_channels+j;
-	  	y.data[tmp] = channel_mean(x.data + i * num_channels * channel_capacity(x) + j * channel_capacity(x)
-	  		, channel_capacity(x));
-	  }
-	return S_OK;
+  for (uint i = 0; i < num_images; ++i)
+    for (uint j = 0; j < num_channels; ++j){
+      y.data[i * num_channels+j] = channel_mean(x.data + i * num_channels * channel_capacity(x) + j * channel_capacity(x)
+        , channel_capacity(x));
+  }
+
+  // create cache
+  if(cache){
+    cache = (lcache_t *) &x;
+  }   
+  
+  return S_OK;
 }
 
 status_t global_avg_pool_backward(tensor_t dx, lcache_t const *cache, tensor_t const dy){
+  tensor_t* t = (tensor_t*)cache;
+  uint N = t->dim.dims[0];
+  uint C = t->dim.dims[1];
+  uint H = t->dim.dims[2];
+  uint W = t->dim.dims[3];
+
+  tensor_t dx = tensor_make_scalar_alike(t, 1/(H*W));
+
+  tensor_op_inplace(dx, dy, TENSOR_OP_MUL);
+
+  return S_OK;
 }
