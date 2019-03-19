d4127905b5f54c60112b731a1d24eef0ac157897
fuyuan <fuyuan@iupui.edu>
2019-03-19 18:26:58 -0400

    seg fault

--- src/layer_pool.c
+++ src/layer_pool.c
@@ -1,13 +1,14 @@
 #include "awnn/layer_pool.h"
 #include "awnn/channel.h"
 
+// y: N, C, 1, 1
 status_t global_avg_pool_forward(tensor_t const x, lcache_t *cache, tensor_t y){
 	uint num_images = x.dim.dims[0];
 	uint num_channels = x.dim.dims[1];
 
 	for (uint i=0; i < num_images; ++i)
 	  for (uint j=0; j < num_channels; ++j){
-	  	channel_mean(x.data + i * num_channels * channel_capacity(x) + j * channel_capacity(x)
+	  	y.data[i*num_channels+j] = channel_mean(x.data + i * num_channels * channel_capacity(x) + j * channel_capacity(x)
 	  		, channel_capacity);
 	  }
 }
