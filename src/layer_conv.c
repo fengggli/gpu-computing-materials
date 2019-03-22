38ba2dc218f733e72792903c4e94d23155cb3fa9
Feng Li <li2251@purdue.edu>
2019-03-22 17:13:01 -0400

    [layer-conv]: improved the test.

--- src/layer_conv.c
+++ src/layer_conv.c
@@ -2,8 +2,26 @@
 
 
 status_t convolution_forward(tensor_t const x, tensor_t const w, lcache_t * cache, conv_param_t const params, tensor_t y){
+  status_t ret = S_ERR;
+
+  // im2col with input
+
+  // im2col with kernels
+
+  // matmul
+
+  // col2img
+
+  ret = S_OK;
+
+  return ret;
 }
 
 
 status_t convolution_backward(tensor_t dx, tensor_t dw, lcache_t const *cache, tensor_t const dy){
+  status_t ret = S_ERR;
+
+  ret = S_OK;
+
+  return ret;
 }
