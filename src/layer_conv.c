4971ebb66b9be5979d7911daf8802ae0dc8bd4a2
fuyuan <fuyuan@iupui.edu>
2019-03-26 17:33:11 -0400

    add im2col_inner

--- src/layer_conv.c
+++ src/layer_conv.c
@@ -1,5 +1,24 @@
 #include "awnn/layer_conv.h"
 
+status_t im2col_inner(tensor_t cols, tensor_t x_padded,
+                        uint N,  uint C,  uint H,  uint W,  uint HH, uint WW,
+                        uint field_height, uint field_width, uint padding, uint stride){
+
+  for (uint c = 0; c < C; c++)
+    for (uint yy = 0; yy < HH; yy++)
+      for (uint xx = 0; xx < WW; xx++)
+        for (uint ii = 0; ii < field_height; ii++)
+          for (uint jj = 0; jj < field_width; jj++){
+            uint row = c * field_width * field_height + ii * field_height + jj;
+            for (uint i = 0; i < N; i++){
+              uint col = yy * WW * N + xx * N + i;
+              cols.data[row * N + col] = x_padded[i, c, stride * yy + ii, stride * xx + jj]; // Correct?
+            }
+          }
+
+  return S_OK;
+}
+
 status_t im2col(tensor_t const x, tensor_t const w, lcache_t * cache, conv_param_t const params, tensor_t out) {
   uint N, C, H, W, num_filters, filter_height, filter_width, stride, pad, x_cols, res;
   N = x.dim.dims[0];
@@ -36,18 +55,20 @@ status_t im2col(tensor_t const x, tensor_t const w, lcache_t * cache, conv_param
 status_t convolution_forward(tensor_t const x, tensor_t const w, lcache_t * cache, conv_param_t const params, tensor_t y){
   status_t ret = S_ERR;
 
-  // im2col with input
+  // 1. flatten the input into vectors which represent the filters
   ret = im2col(x, w, cache, params, y);
 
+  // 2. this is where the filters are actually applied
+  tensor_t res = w.reshape((w.shape[0], -1)).dot(x_cols);
 
+  //##### convert output back to appropriate shape
+  out = res.reshape(w.shape[0], out.shape[2], out.shape[3], x.shape[0]);
 
-  // im2col with kernels
+  // 3. transpose output
+  out = out.transpose(3, 0, 1, 2);
 
-  // matmul
-
-  // col2img
-
-  ret = S_OK;
+  // fill cache
+  cache = (x, w, conv_param, x_cols);
 
   return ret;
 }
