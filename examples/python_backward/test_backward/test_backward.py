e7676b21d1a79b4cee2cbc23de23a75b4ac6dd6d
Christopher Goebel <cmgoebel@iupui.edu>
2019-03-26 16:07:41 -0400

    updates forward example with notes and slight reorg

--- examples/python_backward/test_backward/test_backward.py
+++ examples/python_backward/test_backward/test_backward.py
@@ -1,15 +1,12 @@
 from unittest import TestCase
 import numpy as np
 
-from python_backward.backward import conv_backward_im2col
-from python_backward.test_backward import cache
 from python_forward.forward import conv_forward_im2col
 
 
 class TestConvBackwardIm2col(TestCase):
     def test_conv_backward_im2col(self):
-
-        params = {
+        conv_params = {
             'stride': 2,
             'pad': 1
         }
@@ -20,14 +17,15 @@ class TestConvBackwardIm2col(TestCase):
         sz_filter = 4;
         nr_filter = 3;
 
-        y = [0.02553947, 0.01900658, -0.03984868, -0.09432237,
-             0.05964474, 0.09894079, 0.12641447, 0.19823684,
-             0.09375, 0.178875, 0.29267763, 0.49079605,
-             -0.36098684, -0.57783553, -0.67079605, -1.06632237,
-             0.28701316, 0.42294079, 0.41630921, 0.6075,
-             0.93501316, 1.42371711, 1.50341447, 2.28132237]
+        x_size = nr_img * nr_in_channel * sz_img * sz_img
+        w_size = nr_filter * nr_in_channel * sz_filter * sz_filter
 
-        sz_out = 1 + (sz_img + 2 * params['pad'] - sz_filter) / params['stride']
+        x = np.linspace(-.1, .5, x_size).reshape(nr_img, nr_in_channel, sz_img, sz_img)
+        w = np.linspace(-0.2, 0.3, w_size).reshape(nr_filter, nr_in_channel, sz_filter, sz_filter)
+
+        y, cache = conv_forward_im2col(x, w, conv_param=conv_params)
+
+        sz_out = int(1 + (sz_img + 2 * conv_params['pad'] - sz_filter) / conv_params['stride'])
         shape_y = (nr_img, nr_filter, sz_out, sz_out) # 4x2x5x5
 
         x_size = nr_img * nr_in_channel * sz_img * sz_img
--- examples/python_forward/forward.py
+++ examples/python_forward/forward.py
@@ -1,11 +1,6 @@
 import numpy as np
 
-
-def conv_forward_im2col(x, w, conv_param):
-    """
-    A fast implementation of the forward pass for a convolutional layer
-    based on im2col and col2im.
-    """
+def get_flattened_x(x: np.array, w: np.array, conv_param: dict):
 
     ##### Create output storage (in our C code, this is done in the test)
     N, C, H, W = x.shape
@@ -15,12 +10,6 @@ def conv_forward_im2col(x, w, conv_param):
     # Check dimensions
     assert (W + 2 * pad - filter_width) % stride == 0, 'width does not work'
     assert (H + 2 * pad - filter_height) % stride == 0, 'height does not work'
-
-    # actually create the storage and fill it with 0's
-    out_height = (H + 2 * pad - filter_height) // stride + 1
-    out_width = (W + 2 * pad - filter_width) // stride + 1
-    out = np.zeros((N, num_filters, out_height, out_width), dtype=x.dtype)
-
 
     ##### convert the image to the flattened form
     # note that the length of the rows will be the total size of a filter.
@@ -37,20 +26,43 @@ def conv_forward_im2col(x, w, conv_param):
     #        I would like to explore doing this mapping in CUDA during
     #        the transfer.
     x_cols = im2col_cython(x, w.shape[2], w.shape[3], pad, stride)
+    return x_cols
 
 
-    ##### this is where the filters are actually applied
 
+def conv_forward_im2col(x: np.array, w: np.array, conv_param: dict):
+    """
+    A fast implementation of the forward pass for a convolutional layer
+    based on im2col and col2im.
+    """
+    ############## 1. flatten the input into vectors which represent the filters
+    x_cols = get_flattened_x(x, w, conv_param)
+
+    print(x_cols)
+
+    # setup output buffer
+    N, C, H, W = x.shape
+    num_filters, _, filter_height, filter_width = w.shape
+    stride, pad = conv_param['stride'], conv_param['pad']
+
+    # actually create the storage and fill it with 0's
+    out_height = (H + 2 * pad - filter_height) // stride + 1
+    out_width = (W + 2 * pad - filter_width) // stride + 1
+    out = np.zeros((N, num_filters, out_height, out_width), dtype=x.dtype)
+
+
+    ############### 2. this is where the filters are actually applied
     # the -1 will make the shape into a single column vector of length w.shape[0]
     # this just puts all the weights into a vector.  Then we do a numpy "dot" on the
     # reshaped filters and the x which was placed into rows that were resized based
     # on the filter size.
     res = w.reshape((w.shape[0], -1)).dot(x_cols)
 
+
     ##### convert output back to appropriate shape
     out = res.reshape(w.shape[0], out.shape[2], out.shape[3], x.shape[0])
 
-    ##### transpose output (not sure what this is doing ??????)
+    ############### 3.  transpose output (not sure what this is doing ??????)
     out = out.transpose(3, 0, 1, 2)
 
     ##### fill cache
@@ -59,24 +71,25 @@ def conv_forward_im2col(x, w, conv_param):
     return out, cache
 
 
-def im2col_cython(x, field_height, field_width, padding, stride):
+def im2col_cython(x, filter_height, filter_width, padding, stride):
 
     N = x.shape[0]
     C = x.shape[1]
     H = x.shape[2]
     W = x.shape[3]
 
-    HH = int((H + 2 * padding - field_height) / stride + 1)
-    WW = int((W + 2 * padding - field_width) / stride + 1)
+    HH = int((H + 2 * padding - filter_height) / stride + 1)  # number of times filter will be applied in height dimension
+    WW = int((W + 2 * padding - filter_width) / stride + 1)   # number of times filter will be applied in width dimension
 
     p = padding
     x_padded = np.pad(x, ((0, 0), (0, 0), (p, p), (p, p)), mode='constant')
+
+    # when creating our columns array, we are sizing it based on
+    # (Channels * filter size, number of images * total filter applications
+    cols = np.zeros((C * filter_height * filter_width, N * HH * WW), dtype=x.dtype)  # field_height and width are filter
     
-    cols = np.zeros((C * field_height * field_width, N * HH * WW), dtype=x.dtype)
-    
-    # Moving the inner loop to a C function with no bounds checking works, but does
-    # not seem to help performance in any measurable way.
-    im2col_cython_inner(cols, x_padded, N, C, H, W, HH, WW, field_height, field_width, padding, stride)
+    #
+    im2col_cython_inner(cols, x_padded, N, C, H, W, HH, WW, filter_height, filter_width, padding, stride)
 
     return cols
 
@@ -85,8 +98,8 @@ def im2col_cython_inner(cols, x_padded,
                         N,  C,  H,  W,  HH,  WW,
                         field_height, field_width, padding, stride):
 
-    for c in range(C):
-        for yy in range(HH):
+    for c in range(C):  # for each channel
+        for yy in range(HH):  #
             for xx in range(WW):
                 for ii in range(field_height):
                     for jj in range(field_width):
